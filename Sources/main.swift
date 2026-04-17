656e9367341244612eb65082999a611ca26358ed
Jean Antunes <jeanantunes2005@gmail.com>
2026-04-17 15:08:17 +0000

    coloquei prints

--- Sources/main.swift
+++ Sources/main.swift
@@ -19,10 +19,13 @@ let instrutor = Instrutor(
 
 print("=== Cadastro da Academia ===")
 print(aluno.getDescricao())
+print("Nível do aluno: \(aluno.nivel)")
 print("Plano do aluno: \(aluno.plano.nome)")
 print(String(format: "Mensalidade: R$ %.2f", aluno.plano.calcularMensalidade()))
 print("---")
 print(instrutor.getDescricao())
 print("---")
+print("Plano mensal: \(planoMensal.nome)")
+print(String(format: "Valor mensal do plano: R$ %.2f", planoMensal.calcularMensalidade()))
 print("Plano anual: \(planoAnual.nome)")
 print(String(format: "Valor anual com desconto: R$ %.2f", planoAnual.calcularMensalidade()))
